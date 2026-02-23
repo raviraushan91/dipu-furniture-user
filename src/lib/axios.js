92fb16b65499611bd3a9f81745d82e9a8cd7e741
Ravi Raushan Kumar <raushankr3414@gmail.com>
2026-02-24 00:15:36 +0530

    Update axios base URL for production

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api/v1"
-      : "/",
+      : "https://dipu-furniture-backend.onrender.com/api/v1",
   withCredentials: true,
 });
